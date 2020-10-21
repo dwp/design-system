f03de50d12398466317725743a803efb9a6b0313
Simone Duca <simone.duca@gmail.com>
2020-10-21 15:29:47 +0100

    Adds policy and details routes and static pages. Fixes footer link.

---
+++ app/middleware/cookies/cookiePolicy.js
@@ -0,0 +1,21 @@
+const { setCookieChoice } = require('./cookiesConsent');
+
+const renderCookiePolicy = (req, res) => {
+  res.locals.cookieConsent = req.cookies.seen_cookie_message;
+  res.render(
+    'cookies/cookie-policy.njk',
+  );
+};
+
+const cookiePolicyPostHandler = (req, res, next) => {
+  const cookieConsent = req.body['selected-option'];
+  if (!cookieConsent) {
+    res.send('MAKE A SELECTION');
+    next();
+  } else {
+    setCookieChoice(res, cookieConsent);
+    res.redirect(req.originalUrl);
+  }
+};
+
+module.exports = { renderCookiePolicy, cookiePolicyPostHandler };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { renderCookiePolicy, cookiePolicyPostHandler } = require('../middleware/cookies/cookiePolicy');
+const { renderCookieDetails } = require('../middleware/cookies/cookieDetails')
 const { setCookieChoice, getCookieChoice } = require('../utils/cookiesConsent');
 
+
 router.use(getCookieChoice);
+
+router.post('/set-cookie-message', setCookieChoice);
+router.post('/cookies/cookie-policy', cookiePolicyPostHandler, renderCookiePolicy);
 
 router.get('/', function(req, res) {
   res.render('index');
 }); 
 
-router.post('/set-cookie-message', setCookieChoice);
+router.get('/cookies/cookie-policy', renderCookiePolicy);
+router.get('/cookies/cookie-details', renderCookieDetails);
 
 module.exports = router;
